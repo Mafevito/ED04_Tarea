78251c0ae88314c1aab5ab273e613f71ba3ad080
mafevito <mafevito@hotmail.com>
2022-02-14 21:37:19 +0100

    Comentarios JavaDoc agregados y documentación JavaDoc generada

--- src/main/java/cuentas/CCuenta.java
+++ src/main/java/cuentas/CCuenta.java
@@ -1,63 +1,85 @@
 package cuentas;
 
+
+/**
+ * Clase que se encarga de definir un objeto de tipo CCuenta y a la vez de indicar
+ * el tipo de información que contendra, como nombre, cuenta, saldo y tipo interes.
+ * 
+ * @author Maria Fernanda Villarroel Toaque
+ */
 public class CCuenta {
 
     /**
-     * @return the nombre
+     * Metodo que obtiene el nombre del cliente.
+     * 
+     * @return el nombre
      */
     private String getNombre() {
         return nombre;
     }
 
     /**
-     * @param nombre the nombre to set
+     * Metodo que establece el nombre del cliente.
+     * 
+     * @param nombre el nombre a establecer
      */
     private void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
     /**
-     * @return the cuenta
+     * Metodo que obtiene el codigo de la cuenta del cliente.
+     * 
+     * @return la cuenta
      */
     private String getCuenta() {
         return cuenta;
     }
 
     /**
-     * @param cuenta the cuenta to set
+     * Metodo que establece el codigo de la cuenta del cliente.
+     * 
+     * @param cuenta la cuenta a establecer
      */
     private void setCuenta(String cuenta) {
         this.cuenta = cuenta;
     }
 
     /**
-     * @return the saldo
+     * Metodo que obtiene el saldo en la cuenta del cliente.
+     * 
+     * @return el saldo
      */
     private double getSaldo() {
         return saldo;
     }
 
     /**
-     * @param saldo the saldo to set
+     * Metodo que establece el saldo en la cuenta del cliente.
+     * 
+     * @param saldo el saldo a establecer
      */
     private void setSaldo(double saldo) {
         this.saldo = saldo;
     }
 
     /**
-     * @return the tipoInteres
+     * Metodo que obtiene el tipo de interes que tiene la cuenta del cliente.
+     * 
+     * @return el tipoInteres
      */
     private double getTipoInteres() {
         return tipoInteres;
     }
 
     /**
-     * @param tipoInteres the tipoInteres to set
+     * Metodo que establece el tipo de interes que tiene la cuenta del cliente.
+     * 
+     * @param tipoInteres el tipoInteres a establecer
      */
     private void setTipoInteres(double tipoInteres) {
         this.tipoInteres = tipoInteres;
     }
-
 
     private String nombre;
     private String cuenta;
@@ -75,6 +97,15 @@ public class CCuenta {
     {
     }
 
+    /**
+     * Metodo constructor de la clase CCuenta, se encarga de crear fisicamente el objeto en la memoria.
+     * 
+     * @param nom nombre del cliente de la cuenta
+     * @param cue codigo de cuenta del cliente
+     * @param sal saldo disponible en la cuenta
+     * @param tipo tipo de interes en la cuenta del cliente
+     */
+    
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
@@ -82,11 +113,22 @@ public class CCuenta {
         saldo=sal;
     }
 
+    /**
+     * Metodo que devuelve el saldo disponible en la cuenta del cliente.
+     * 
+     * @return devuelve el saldo disponible en la cuenta del cliente
+     */
     public double estado()
     {
         return getSaldo();
     }
 
+    /**
+     * Metodo que sirve para ingresar saldo en la cuenta del cliente.
+     * 
+     * @param cantidad cantidad de saldo a ingresar en la cuenta
+     * @throws Exception excepcion que se captura si la cantidad es menor que 0 
+     */
     public void ingresar(double cantidad) throws Exception
     {
         if (cantidad<0)
@@ -94,6 +136,12 @@ public class CCuenta {
         setSaldo(getSaldo() + cantidad);
     }
 
+    /**
+     * Metodo que sirve para retirar saldo de la cuenta del cliente.
+     * 
+     * @param cantidad cantidad a retirar de la cuenta del cliente
+     * @throws Exception excepcion que se captura si la cantidad a retirar es menor que 0 o si no hay suficiente saldo
+     */
     public void retirar(double cantidad) throws Exception
     {
         if (cantidad <= 0)
